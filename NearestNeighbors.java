50d6954b3813d839317f1b24e2ec0e534d60b656
Sina Moshtaghi <sinamosh@yahoo.de>
2017-04-18 07:07:55 +0200

    Add files via upload

---
+++ NearestNeighbors.java
@@ -0,0 +1,29 @@
+// Hausübung 1 Sina Moshtaghi 
+
+package classifier;
+
+import java.util.ArrayList;
+
+public class NearestNeighbors {
+	private final ArrayList<Object[]> date = new ArrayList<Object[]>();
+	
+	public void learn(int nameLength, int nameDigits, boolean label) {
+		this.data.add(new Object[] {nameLength, nameDigits, label}); 
+		
+	}
+	
+	public boolean classify(String name)
+	    return ((boolean) this.data.get((int) (double)Math.pow((int)n[0] - s.length(), 2) + Math.pow((int, arg1)this.data.get[1] - s.ReplaceAll()
+	
+	
+	
+	
+	String[] names = {"kater", "xmb", "chazette", "12345IchKaufDeinAuto12345","Buy1234Buy1234Buy1234"};
+	
+	for (String string : names) {
+		System.out.println( string + " ist " + (nn.classify(string) ? "ein" : "kein") + " Spammer.");
+	    
+		}
+	}
+
+}
